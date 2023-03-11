eb708e1a55edc7199af6fef5fbd37c0fb147275a
asebeyakob@gmail.com <asebeyakob@gmail.com>
2023-03-11 00:48:20 -0700

    message

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const getMessage = [];
+getMessage=(["Guadelupe", "Ollie","Aki"],"Surprise");
+function writeCards(getMessage){
+    
+    for (let i=0; i < getMessage.length; i++) {
+        console.log(`Thank you, ${getMessage[i]}, for the wonderful ${getMessage} gift!`);
+    return getMessage;
+    }
+    debugger;
+}
+//writeCards(["Guadelupe", "Ollie","Aki"],"Surprise");
+function countDown(){
+    let numCountDown=11;
+    while(numCountDown--){
+        console.log(numCountDown);
+
+    }
+}
