4788a1967035e65fd080b23adc02d4c49596426b
asebeyakob@gmail.com <asebeyakob@gmail.com>
2023-03-20 00:08:23 -0600

    loop

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 // Code your solutions in this file
-const getMessage = [];
-getMessage=(["Guadelupe", "Ollie","Aki"],"Surprise");
-function writeCards(getMessage){
-    
-    for (let i=0; i < getMessage.length; i++) {
-        console.log(`Thank you, ${getMessage[i]}, for the wonderful ${getMessage} gift!`);
-    return getMessage;
+const getMessage=["Guadalupe", "Ollie", "Aki"];
+function writeCards(name,event) {
+    let message=[]
+    for (let i=0; i < name.length; i++){
+        message.push("Thank you, "+name[i] +","+ " for the wonderful " + event + " gift!");
+   debugger;
     }
-    debugger;
-}
+        return  message;
+
+    }
+    console.log ["Guadalupe,", "Ollie,", "Aki,"];
+    console.log(writeCards(names,events));
+    
 //writeCards(["Guadelupe", "Ollie","Aki"],"Surprise");
 function countDown(){
-    let numCountDown=11;
-    while(numCountDown--){
-        console.log(numCountDown);
+    let numCountDown=10;
+    while(numCountDown>=0){
+        console.log(numCountDown--);
 
     }
 }
